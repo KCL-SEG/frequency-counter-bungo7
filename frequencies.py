65a3e8f395affdebabb2b8a26ee42098c2fc40a7
bungo7 <allen.kallikadan_rapheal@kcl.ac.uk>
2022-12-09 04:44:04 +0000

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,12 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    items = list(map(str, items))
+
+    for i in items:
+        if i in frequencies:
+            frequencies[i] += 1
+        else:
+            frequencies[i] = 1
+
     return frequencies
